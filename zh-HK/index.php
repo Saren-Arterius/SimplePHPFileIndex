83dae38e1153e57bf63db973238973a07a646029
Shaorune <ykmran@gmail.com>
2013-12-20 01:32:43 +0800

    Revert "make sure zh-HK's folders always on top, not bottom"
    This reverts commit e4a749790e237425deefc6d447f152c9e0e185b9.

--- zh-HK/index.php
+++ zh-HK/index.php
@@ -517,7 +517,7 @@ $currentDir = currentDir();
                         }
                     },
                     "aaSorting": [
-                        [ 1, "desc" ],
+                        [ 1, "asc" ],
                         [ 0, "asc" ]
                     ],
                     "aoColumns": [
